e994c71daeb5b896e32ebceba8ceb8e2aa7347c5
ianchan3 <ianchan3138@gmail.com>
2022-07-18 15:49:56 -0700

    Add Flights and All Flights Details Appear

--- models/flight.js
+++ models/flight.js
@@ -2,11 +2,38 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const flightSchema = new Schema ({
-  airline: String,
-  airport: String,
-  flightNo: Number,
-  departs: Date
+const destinationSchema = new Schema({
+  airport: {
+    type: String,
+    enum: ["SFO", "DEN", "LAX", "JFK"]
+  },
+  arrival: {
+    type: Date
+  }
+});
+
+const flightSchema = new Schema({
+  airline: {
+      type: String,
+      enum: ['Delta', 'United', 'American', 'Southwest']
+  },
+  airport: {
+      type: String,
+      enum: ['SFO', 'DEN', 'LAX', 'JFK'],
+      default: 'DEN'
+  },
+  flightNo: {
+      type: Number,
+      required: true,
+      min: 10,
+      max: 999
+  },
+  departs: {
+      type: Date,
+      default: Date.now() + 365*24*60*60000
+  },
+  destinations: [destinationSchema],
+  
 });
 
 module.exports = mongoose.model('Flight', flightSchema);
--- routes/flights.js
+++ routes/flights.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 const flightsCtrl = require('../controllers/flights');
 
-// GET /movies (index functionality)
+// GET /flights (index functionality)
 router.get('/', flightsCtrl.index);
-// GET /movies/new (new functionality)
+// GET /flights/new (new functionality)
 router.get('/new', flightsCtrl.new);
+// GET /flights/:id (show functionality)
+router.get('/:id', flightsCtrl.show);
+router.post('/', flightsCtrl.create);
 
 module.exports = router;
 
